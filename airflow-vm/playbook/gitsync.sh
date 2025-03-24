5b1348d3d3c2d837c34dc1e755582dbbb8577570
Ubuntu <god@Projects-Hashlama016-airflow-black-vm.jrnrujkewrbezi5xdrv1md0vng.ax.internal.cloudapp.net>
2025-03-24 15:51:55 +0000

    Added gitsync capabilities

---
+++ airflow-vm/playbook/gitsync.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+REPOS="
+git@github.com:Hashlama-016/airflow-DAGs.git
+git@github.com:Hashlama-016/airflow-team-DAGs.git
+"
+
+for repo in $REPOS; do
+	set -- $repo
+	URL=$1
+	DIR=$(basename "$1")
+
+	echo $URL
+	echo $DIR
+
+	if [ -z "$URL" ] || [ -z "$DIR" ]; then
+		echo skipping
+		continue
+	fi
+
+	if [ ! -d "/opt/airflow/dags/$DIR/.git" ]; then
+		echo "Cloning $URL..."
+		git clone --depth 1 "$URL" "/opt/airflow/dags/$DIR"
+	else
+		echo "Updating $URL..."
+		cd "/opt/airflow/dags/$DIR" && git pull origin main
+	fi
+done
