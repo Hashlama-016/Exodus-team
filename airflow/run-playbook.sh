cfc05f3f9927cea3fee875b6407757171c875579
Ubuntu <god@Projects-Hashlama016-airflow-black-vm.jrnrujkewrbezi5xdrv1md0vng.ax.internal.cloudapp.net>
2025-03-25 09:14:32 +0000

    Removed unneeded files again

--- airflow/run-playbook.sh
+++
@@ -1,1 +0,0 @@
-ansible-playbook airflow-playbook.yaml --extra-vars "url=https://raw.githubusercontent.com/Hashlama-016/Exodus-team/refs/heads/feat/airflow-containered-postgres/airflow-vm/docker-compose.yaml"
