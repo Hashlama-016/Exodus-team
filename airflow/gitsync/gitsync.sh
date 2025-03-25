3fd736c63e36cef5e92f7ec4ff8d022d50ec68c7
Penthar <124145941+Penthar@users.noreply.github.com>
2025-03-25 11:21:47 +0200

    Update gitsync.sh

--- airflow/gitsync/gitsync.sh
+++ airflow/gitsync/gitsync.sh
@@ -8,6 +8,7 @@ git@github.com:Hashlama-016/airflow-DAGs.git
 git@github.com:Hashlama-016/airflow-team-DAGs.git
 git@github.com:Hashlama-016/Obelisk-DAGs.git
 http://10.50.1.4/Hashlama_8/airflow-test.git
+http://10.50.1.4/Hashlama_8/obelisk-airflow.git
 "
 
 for repo in $REPOS; do
