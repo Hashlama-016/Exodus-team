67b51875af0cac4030874df964d83ead5cfacf85
Ubuntu <god@Projects-Hashlama016-airflow-black-vm.jrnrujkewrbezi5xdrv1md0vng.ax.internal.cloudapp.net>
2025-03-25 09:06:48 +0000

    Fixed history

---
+++ airflow/run-playbook.sh
@@ -0,0 +1,1 @@
+ansible-playbook airflow-playbook.yaml --extra-vars "url=https://raw.githubusercontent.com/Hashlama-016/Exodus-team/refs/heads/feat/airflow-containered-postgres/airflow-vm/docker-compose.yaml"
