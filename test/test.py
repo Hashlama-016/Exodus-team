fe061fa2eb7a1223ee058614629001c3a0cc6dc5
nevo11 <nevo42668@gmail.com>
2025-03-25 11:05:21 +0200

    beter test

--- test/test.py
+++ test/test.py
@@ -5,14 +5,17 @@ import time
 dotenv.load_dotenv()
 url_model_run = os.getenv("url_model_run")
 url_model_classes = os.getenv("url_model_classes")
+url_model_s3_run = os.getenv("url_model_s3_run")
 
 
-
-data = {'classes': [0]}
+params = {'classes': [0]}
 
 files = {'file': open(os.getenv("file"), 'rb')}
-response = requests.post(url_model_run, files=files, json=data, headers={'x-key': os.getenv("token_key")})
+response = requests.post(url_model_run, files=files, params=params, headers={'x-key': os.getenv("token_key")})
 print(response.json())
-response = requests.get(url_model_classes, files=files, json=data, headers={'x-key': os.getenv("token_key")})
+response = requests.get(url_model_classes, headers={'x-key': os.getenv("token_key")})
+print(response.json())
+params = {'classes': [0], 's3_path': os.getenv("s3_path")}
+response = requests.post(url_model_s3_run, params=params, headers={'x-key': os.getenv("token_key")})
 print(response.json())
 
